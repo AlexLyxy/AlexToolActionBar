c9a67697d99ebfdc8e0b0d3f6869df8a953aeb4f
Oleksandr Lysenko <alexlyxy@gmail.com>
2023-10-22 01:53:20 +0300

    Initial commit

---
+++ app/src/main/java/com/alexlyxy/alextoolactionbar/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.alexlyxy.alextoolactionbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Alex"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // if(item.itemId == android.R.id.home) finish()         If One Item
+        when (item.itemId) {
+           // android.R.id.home -> finish()
+            android.R.id.home -> {
+                Toast.makeText(this, "HOME", Toast.LENGTH_LONG).show()
+            }
+            R.id.sync -> {
+                Toast.makeText(this, "SYNC", Toast.LENGTH_LONG).show()
+            }
+            R.id.search -> {
+                Toast.makeText(this, "SEARCH", Toast.LENGTH_LONG).show()
+
+            }
+            R.id.save -> {
+                Toast.makeText(this, "SAVE", Toast.LENGTH_LONG).show()
+
+            }
+            R.id.delete -> {
+                Toast.makeText(this, "DELETE", Toast.LENGTH_LONG).show()
+            }
+        }
+        return true
+    }
+}

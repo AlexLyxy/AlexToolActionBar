4656486407885c7741e7928c07729599076df60e
Oleksandr Lysenko <alexlyxy@gmail.com>
2023-11-07 19:49:42 +0200

    Last version 07.11.2023

--- app/src/main/java/com/alexlyxy/alextoolactionbar/MainActivity.kt
+++ app/src/main/java/com/alexlyxy/alextoolactionbar/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.alexlyxy.alextoolactionbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.title = "Alex"
+        supportActionBar?.title = "AlexLyxy"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
